bdc4e1292bb0529f2130289e1998e86ce510fb01
JaceRiehl <jace.riehl@gmail.com>
2018-06-11 11:46:55 -0600

    The website is now currently working with npm dev server but not the firebase dev server.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import firebase from 'firebase'
+import VueFire from 'vuefire'
 import { store }  from './store/store'
+
+Vue.use(VueFire);
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyABctVOAOi6xpr_5cF6Rp93MjsO1Uj-jJc",
+  authDomain: "programming-wars.firebaseapp.com",
+  databaseURL: "https://programming-wars.firebaseio.com",
+  projectId: "programming-wars",
+  storageBucket: "programming-wars.appspot.com",
+  messagingSenderId: "543791998135"
+};
+firebase.initializeApp(config);
+
+//Vue.http.options.root = 'https://programming-wars.firebaseio.com';
+
 
 /* eslint-disable no-new */
 new Vue({
