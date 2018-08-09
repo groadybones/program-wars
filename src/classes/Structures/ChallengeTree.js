e1454ab169dedd237894904e1c3a5def95d127a7
JaceRiehl <jace.riehl@gmail.com>
2018-08-09 12:59:46 -0600

    Committing to backup before changing algorithm

--- src/classes/Structures/ChallengeTree.js
+++ src/classes/Structures/ChallengeTree.js
@@ -15,12 +15,14 @@ export default class Tree {
     let level = -1
     console.log(JSON.stringify(this.currentNode.value))
     console.log('length ' + JSON.stringify(this.currentNode.value.cards.length))
+    // See if theres any instructions in the hand
     if (level === -1) {
       instIndices = this.instructionsIndices()
       ++level
     } else {
       instIndices = undefined
     }
+    // if theres any instruction add their values into the roots 'scores' array
     if (instIndices !== undefined) {
       if (instIndices.length !== 0) {
         for (let c = instIndices.length - 1; c >= 0; c--) {
@@ -33,22 +35,39 @@ export default class Tree {
         console.log('to be implemented, maybe return false') // needs to redraw
       }
     }
-    while (this.currentNode.value.cards === []) {
-      for (let node in this.parentNode.value.children) {
-        console.log('node: ' + node)
-
-        // console.log('value: ' + JSON.stringify(this.currentNode.value))
-        for (let score in this.currentNode.value.scores) {
-          for (let remainingCard in this.currentNode.value.cards) {
-            let tempScores = this.currentNode.value.scores
-            tempScores[score] += this.currentNode.value.cards[remainingCard].value
-            this.currentNode.value.children.push(new Node(this.currentNode.value.cards.splice(remainingCard, 1), tempScores))
+    // create the first children of the root
+    if (this.parentNode.children.length === 0) {
+      for (let score in this.currentNode.value.scores) {
+        for (let remainingCard in this.currentNode.value.cards) {
+          let tempScores = this.currentNode.value.scores.slice(0)
+          tempScores[score] += this.currentNode.value.cards[remainingCard].value
+          this.currentNode.children.push(new Node(this.currentNode.value.cards.slice(remainingCard), tempScores))
+        }
+      }
+    }
+    console.log('parent: ' + JSON.stringify(this.parentNode))
+    while (this.currentNode.value.cards.length !== 0) {
+      let tempParent = this.parentNode
+      for (let child in tempParent.children) {
+        // console.log('in the while loop')
+        for (let node in this.parentNode.children) {
+          console.log('node: ' + node)
+          this.currentNode = this.parentNode.children[node]
+          // console.log('value: ' + JSON.stringify(this.currentNode.value))
+          for (let score in this.currentNode.value.scores) {
+            console.log('current score: ' + score)
+            for (let remainingCard in this.currentNode.value.cards) {
+              console.log('remaining card: ' + JSON.stringify(this.currentNode.value.cards[remainingCard].value))
+              let tempScores = this.currentNode.value.scores
+              tempScores[score] += this.currentNode.value.cards[remainingCard].value
+              this.currentNode.children.push(new Node(this.currentNode.value.cards.slice(remainingCard), tempScores))
+            }
           }
         }
-        this.parentNode = this.currentNode
+        this.parentNode = tempParent.children[child]
+        console.log('scores: ' + this.currentNode.value.scores)
+        console.log('currentNode: ' + JSON.stringify(this.currentNode.children))
       }
-      console.log('scores: ' + this.currentNode.value.scores)
-      ++level
     }
   }
 
